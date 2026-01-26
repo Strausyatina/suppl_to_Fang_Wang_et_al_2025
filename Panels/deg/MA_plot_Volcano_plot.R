00ebb91bec9d755ecaec23916a6417ac8d6c65ae
Strausyatina <asya.mendelevich@gmail.com>
2026-01-26 17:21:11 +0100

    deg - ma/volcano - upd

--- Panels/deg/MA_plot_Volcano_plot.R
+++ Panels/deg/MA_plot_Volcano_plot.R
@@ -1,7 +1,7 @@
 library(ggplot2)
 
 #set working directory to source file directory as all paths are relative to it
-gwdir<-getSrcDirectory(function(){})[1]
+gwdir<-getwd() #getSrcDirectory(function(){})[1]
 setwd(gwdir)
 getwd()
 
@@ -16,7 +16,6 @@ edgeR_result_files<-dir("../../RNA-seq/DEG/data/",pattern="*FC2_fdr05.tsv",full.
 edgeR_result_files<-edgeR_result_files[grepl("MLL",edgeR_result_files)]
 edgeR_resList<-c(edgeR_resList,lapply(edgeR_result_files,function(X)read.table(X,sep="\t",quote="",header=TRUE)))
 names(edgeR_resList)[3:5]<-paste0("GV_",gsub("\\.FC2","",unlist(lapply(strsplit(basename(edgeR_result_files),split="_"),function(X)X[7]))))
-
 
 
 set_custom_wd<-function(label){
@@ -35,9 +34,66 @@ recalc_change_cats<-function(table){
   return(table)
 }
 
+PLOT_BASE_SIZE <- 18
+PLOT_THEME <- function(){
+  theme_classic(base_size = PLOT_BASE_SIZE) +
+    theme(
+      legend.position = "right",
+      legend.title = element_blank(),
+      legend.key = element_blank(),
+      legend.margin = margin(6, 6, 6, 6),
+      axis.title = element_text(size = PLOT_BASE_SIZE, face = "bold"),
+      axis.text = element_text(size = PLOT_BASE_SIZE-2),
+      plot.title = element_text(hjust = 0.5, size = PLOT_BASE_SIZE+2, face = "bold"),
+      plot.margin = margin(10, 10, 10, 10),
+      aspect.ratio = 1,
+      panel.border = element_rect(fill = NA, colour = "black", linewidth = 0.8),
+      axis.line = element_blank()
+    )
+}
+CHANGE_LEVELS <- c(
+  "LFC>=1 and FDR<0.05",
+  "LFC<=-1 and FDR<0.05",
+  "1>LFC>0 and FDR<0.05",
+  "-1<LFC<0 and FDR<0.05",
+  "none"
+)
+get_change_color_map <- function(){
+  c(
+    'LFC>=1 and FDR<0.05' = '#D15FEE',
+    'LFC<=-1 and FDR<0.05' = '#4575B4',
+    '1>LFC>0 and FDR<0.05' = colorspace::lighten('#D15FEE',amount=0.5),
+    '-1<LFC<0 and FDR<0.05' = colorspace::lighten("#4575B4",amount=0.5),
+    'none' = 'grey'
+  )
+}
+
+add_group_counts <- function(p, table, x_pos){
+  colmap <- get_change_color_map()
+  cnt <- table(table$change_recalc)
+  up_key   <- "LFC>=1 and FDR<0.05" # leaving only those
+  down_key <- "LFC<=-1 and FDR<0.05" # leaving only those
+  n_up   <- ifelse(up_key   %in% names(cnt), as.integer(cnt[up_key]),   0)
+  n_down <- ifelse(down_key %in% names(cnt), as.integer(cnt[down_key]), 0)
+  p <- p +
+    annotate("text",
+             x = x_pos, y = 10,
+             label = paste("Up:", n_up),
+             color = colmap[up_key],
+             hjust = 0, vjust = 1, size = 5.5) +
+    annotate("text",
+             x = x_pos, y = -9,
+             label = paste("Down:", n_down),
+             color = colmap[down_key],
+             hjust = 0, vjust = 1, size = 5.5)
+  return(p)
+}
+
 
 plot_custom_MA<-function(table,label){
   table<-recalc_change_cats(table)
+  table$change_recalc <- factor(table$change_recalc, levels = CHANGE_LEVELS)
+  
   if(grepl("MLL",label)){
     if(grepl("MLL34",label)){
       label_to_title<-"Mll3/4 cKO"
@@ -45,35 +101,28 @@ plot_custom_MA<-function(table,label){
   }else{
     label_to_title<-sprintf("%s stage oocyte",gsub("_.+","",label))
   }
+  colmap <- get_change_color_map()
   p.ma <- ggplot(table, aes(logCPM, logFC)) +
     geom_point(aes(color = change_recalc), show.legend = TRUE, size = 1, alpha = 1) +  # Adjust point size and transparency
     geom_hline(yintercept = 0, linetype = "dashed", color = "black") +  # Horizontal line at log2FoldChange = 0
-    scale_color_manual(values = c('LFC>=1 and FDR<0.05' = '#D15FEE', 'LFC<=-1 and FDR<0.05' = '#4575B4',
-                                  '1>LFC>0 and FDR<0.05'=colorspace::lighten('#D15FEE',amount=0.5),'-1<LFC<0 and FDR<0.05'=colorspace::lighten("#4575B4",amount=0.5), 'none' = 'grey'),
-                       guide = guide_legend(nrow=3,ncol=2))+
-    labs(x = 'logCPM', y = 'log2FoldChange') +
-    ylim(-12, 12) +
-    theme_classic() +
-    theme(
-      legend.position = "top",  # Place legend at the top
-      legend.title = element_blank(),  # No legend title
-      legend.key = element_blank(),  # No background for legend keys
-      legend.margin = margin(6, 6, 6, 6),
-      axis.title = element_text(size = 12),  # Larger axis titles
-      axis.text = element_text(size = 10),  # Larger axis labels
-      plot.title = element_text(hjust = 0.5, size = 14, face = "bold"),  # Centered, bold title
-      plot.margin = margin(10, 10, 10, 10), # Add margins to the plot
-      aspect.ratio=1
-    ) + ggtitle(sprintf("MA Plot: Differentially expressed genes \n in %s samples.",label_to_title))+
-    geom_hline(yintercept=1,linetype = "dashed", color = "grey80", linewidth = 0.3)+geom_hline(yintercept=(-1),linetype = "dashed", color = "grey80", linewidth = 0.3)
+    scale_color_manual(values = colmap, drop = FALSE, guide = guide_legend(ncol=1)) +
+    labs(x = 'log2 CPM', y = 'log2 Fold Change (cKO/WT)') +
+    ylim(-11, 11) +
+    PLOT_THEME() + 
+    ggtitle(sprintf("MA Plot\nDifferentially expressed genes in \n%s",label_to_title)) +
+    geom_hline(yintercept=1,linetype = "dashed", color = "grey80", linewidth = 0.3)+
+    geom_hline(yintercept=(-1),linetype = "dashed", color = "grey80", linewidth = 0.3)
   
   # Save the plot to a file
-    ggsave(paste0(label, '.ma_plot.png'), p.ma, width = 6, height = 6)
-    ggsave(paste0(label, '.ma_plot.pdf'), p.ma, width = 6, height = 6)
+  p.ma <- add_group_counts(p.ma, table, x_pos = 0)
+  ggsave(paste0(label, '.ma_plot.png'), p.ma, width = 8, height = 6)
+  ggsave(paste0(label, '.ma_plot.pdf'), p.ma, width = 8, height = 6)
 }
 
 plot_custom_Volcano<-function(table,label){
   table<-recalc_change_cats(table)
+  table$change_recalc <- factor(table$change_recalc, levels = CHANGE_LEVELS)
+  
   if(grepl("MLL",label)){
     if(grepl("MLL34",label)){
       label_to_title<-"Mll3/4 cKO"
@@ -81,35 +130,22 @@ plot_custom_Volcano<-function(table,label){
   }else{
     label_to_title<-sprintf("%s stage oocyte",gsub("_.+","",label))
   }
-    p.volcano <- ggplot(table, aes(logFC, -log10(FDR))) +
+  colmap <- get_change_color_map()
+  p.volcano <- ggplot(table, aes(logFC, -log10(FDR))) +
     geom_point(aes(color = change_recalc), show.legend = TRUE, size = 1, alpha = 1) +  # Adjust size and transparency
     geom_vline(xintercept = 0, linetype = "dashed", color = "black") +  # Vertical line at x = 0
-    scale_color_manual(values = c('LFC>=1 and FDR<0.05' = '#D15FEE', 'LFC<=-1 and FDR<0.05' = '#4575B4',
-                                  '1>LFC>0 and FDR<0.05'=colorspace::lighten('#D15FEE',amount=0.5),'-1<LFC<0 and FDR<0.05'=colorspace::lighten("#4575B4",amount=0.5), 'none' = 'grey'),
-                       guide = guide_legend(nrow=3,ncol=2)) +  # Muted red, blue, and gray
-    labs( x = 'log2 Fold Change', y = '-log10(FDR)') +
-    theme_classic() +
-    xlim(-12, 12) +
-    ylim(0, 50) +
-    theme(
-      legend.position = "top",  # Place the legend at the top
-      legend.title = element_blank(),  # No title for the legend
-      legend.key = element_blank(),  # No background for the legend keys
-      legend.margin = margin(6, 6, 6, 6),
-      axis.title = element_text(size = 12),  # Larger axis titles
-      axis.text = element_text(size = 10),  # Larger axis labels
-      plot.title = element_text(hjust = 0.5, size = 14, face = "bold"),  # Centered and bold title
-      plot.margin = margin(10, 10, 10, 10),  # Add margins to the plot
-      aspect.ratio=1
-    ) + ggtitle(sprintf("Volcano Plot: Differentially expressed genes \n in %s samples.",label_to_title))+
-      geom_vline(xintercept=1,linetype = "dashed", color = "grey80", linewidth = 0.3)+geom_vline(xintercept=(-1),linetype = "dashed", color = "grey80", linewidth = 0.3)+
-      geom_hline(yintercept=-log10(0.05),linetype = "dashed", color = "grey80", linewidth = 0.3)
-  
-  p.volcano 
+    scale_color_manual(values = colmap, drop = FALSE, guide = guide_legend(ncol=1)) +  # Muted red, blue, and gray
+    labs( x = 'log2 Fold Change (cKO/WT)', y = '-log10(FDR)') +
+    xlim(-11, 11) +
+    ylim(0, 11) +
+    PLOT_THEME() + 
+    ggtitle(sprintf("Volcano Plot\nDifferentially expressed genes in \n%s",label_to_title))+
+    geom_vline(xintercept=1,linetype = "dashed", color = "grey80", linewidth = 0.3)+geom_vline(xintercept=(-1),linetype = "dashed", color = "grey80", linewidth = 0.3)+
+    geom_hline(yintercept=-log10(0.05),linetype = "dashed", color = "grey80", linewidth = 0.3)
   
   # Save the plot to a file
-  ggsave(paste0(label, '.volcano_plot.png'), p.volcano, width = 6, height = 6)
-  ggsave(paste0(label, '.volcano_plot.pdf'), p.volcano, width = 6, height = 6)
+  ggsave(paste0(label, '.volcano_plot.png'), p.volcano, width = 8, height = 6)
+  ggsave(paste0(label, '.volcano_plot.pdf'), p.volcano, width = 8, height = 6)
   
 }  
 
@@ -122,7 +158,6 @@ produce_results<-function(table,label){
 
 mapply(FUN=function(X,Y)produce_results(X,Y),edgeR_resList,names(edgeR_resList))
 
-gwdir<-getSrcDirectory(function(){})[1]
 setwd(gwdir)
 getwd()
 
